13249ae5c32ad1e8757f0afefe23e2aac6d53dc0
Pawan <148438001+Pawanchandraupreti@users.noreply.github.com>
2024-08-30 22:17:20 +0530

    Add files via upload

---
+++ Day5/Day5_question3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void countVowelsConsonants(string str) {
+    int vowels = 0, consonants = 0;
+    for (char ch : str) {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            vowels++;
+        } else if (ch >= 'a' && ch <= 'z') {
+            consonants++;
+        }
+    }
+    cout << "Number of vowels: " << vowels << endl;
+    cout << "Number of consonants: " << consonants << endl;
+}
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    cin >> input;
+    countVowelsConsonants(input);
+    return 0;
+}
